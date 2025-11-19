2ad34d4bf157a9543ec3d435e7cc7fb00024c5d6
MarkGamboaa <gamboamarkgerard@gmail.com>
2025-11-19 20:45:54 +0800

    Initial Commit

---
+++ Notes/NoteViewController.swift
@@ -0,0 +1,36 @@
+//
+//  NoteViewController.swift
+//  Lumen
+//
+//  Created by Mark Gerard G. Gamboa on 11/14/25.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var noteLabel: UITextView!
+
+    public var noteTitle: String = ""
+    public var note: String = ""
+    public var onUpdate: ((String, String) -> Void)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleLabel.text = noteTitle
+        noteLabel.text = note
+
+        titleLabel.isUserInteractionEnabled = true
+        noteLabel.isEditable = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        let updatedTitle = titleLabel.text ?? ""
+        let updatedNote = noteLabel.text ?? ""
+        onUpdate?(updatedTitle, updatedNote)
+    }
+}
---
+++ Notes/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  Lumen
+//
+//  Created by Mark Gerard G. Gamboa on 11/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var table: UITableView!
+    @IBOutlet var label: UILabel!
+
+    var models: [(title: String, note: String)] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        table.delegate = self
+        table.dataSource = self
+        title = "Lumen"
+    }
+
+    @IBAction func didTapNewNote() {
+        guard let vc = storyboard?.instantiateViewController(identifier: "new") as? EntryViewController else {
+            return
+        }
+        vc.title = "New Note"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        vc.completion = { noteTitle, note in
+            self.navigationController?.popToRootViewController(animated: true)
+            self.models.append((title: noteTitle, note: note))
+            self.label.isHidden = true
+            self.table.isHidden = false
+
+            self.table.reloadData()
+        }
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+    // Table
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row].title
+        cell.detailTextLabel?.text = models[indexPath.row].note
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        let model = models[indexPath.row]
+
+        // Show note controller
+        guard let vc = storyboard?.instantiateViewController(identifier: "note") as? NoteViewController else {
+            return
+        }
+        vc.navigationItem.largeTitleDisplayMode = .never
+        vc.title = "Note"
+        vc.noteTitle = model.title
+        vc.note = model.note
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+    // MARK: - Swipe-to-Delete
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Remove the item from the data source
+            models.remove(at: indexPath.row)
+            
+            // Delete the row from the table view with animation
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+}
